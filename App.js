32b028ba20e10fe91575896155c8a72eb69830fb
Rodrigo Zanotta <rodrigozanottarsz@gmail.com>
2020-01-09 13:19:30 -0300

    Notification works!

--- App.js
+++ App.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import { StyleSheet, Text, View, Alert } from 'react-native';
 import { Notifications } from 'expo';
-import * as Permisissions from 'expo-permissions';
+import * as Permissions from 'expo-permissions';
 import { Platform } from 'react-native'; 
-
+import { initnotify, getToken } from 'expo-push-notification-helper';
 
 class App extends React.Component{
 
-    componentWillMount(){
-        // get expo push token
-        let token = Notifications.getExpoPushTokenAsync();
+    componentDidMount(){
 
-        Alert.alert('Token', token);
+        /*const { status: existingStatus } = Permissions.getAsync(
+            Permissions.NOTIFICATIONS
+        );
+
+        let finalStatus = existingStatus;
+
+        // only ask if permissions have not already been determined, because
+        // iOS won't necessarily prompt the user a second time.
+        if (existingStatus !== 'granted') {
+            // Android remote notification permissions are granted during the app
+            // install, so this will only ask on iOS
+            const { status } = Permissions.askAsync(Permissions.NOTIFICATIONS);
+            finalStatus = status;
+        }
+
+        // Stop here if the user did not grant permissions
+        if (finalStatus !== 'granted') {
+            Alert.alert('Token', 'Não deu permissão');
+            return;
+        }
+
+        // get expo push token
+        let token = Notifications.getExpoPushTokenAsync();*/
+
+        initnotify().then(async (data) => {
+            if (data) {
+                Alert.alert('Token', await getToken());
+            } else {
+                Alert.alert('Token', 'Sem permissão');
+            }
+
+            // Alert.alert('Token', 'Final code');
+
+        })
+
+        
     } 
 
     render(){
         return (
             <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!!!</Text>
+                <Text>React Notification</Text>
             </View>
         );
     }
